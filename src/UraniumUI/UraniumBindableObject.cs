31b4617ee43da82d14bfcf4ee2eac46baed2a902
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-07 19:15:35 +0300

    DataGrid collection changed optimization

---
+++ src/UraniumUI/UraniumBindableObject.cs
@@ -0,0 +1,14 @@
+﻿namespace UraniumUI;
+public abstract class UraniumBindableObject : BindableObject
+{
+    public void SetProperty<T>(ref T field, T value, [System.Runtime.CompilerServices.CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(field, value))
+        {
+            return;
+        }
+
+        field = value;
+        OnPropertyChanged(propertyName);
+    }
+}

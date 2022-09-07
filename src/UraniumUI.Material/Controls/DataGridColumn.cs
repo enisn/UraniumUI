e472be5a1c454b0314982d80d50ed27b7543ac91
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-07 19:51:26 +0300

    Add Width to DataGrid Columns

--- src/UraniumUI.Material/Controls/DataGridColumn.cs
+++ src/UraniumUI.Material/Controls/DataGridColumn.cs
@@ -1,10 +1,17 @@
-﻿using System.Reflection;
+﻿using System.ComponentModel;
+using System.Reflection;
 
 namespace UraniumUI.Material.Controls;
 public class DataGridColumn
 {
     public string Title { get; set; }
+
     public DataTemplate CellItemTemplate { get; set; }
+
     public string PropertyName { get; set; }
+
+    [TypeConverter(typeof(GridLengthTypeConverter))]
+    public GridLength Width { get; set; } = GridLength.Auto;
+
     internal PropertyInfo PropertyInfo { get; set; }
 }

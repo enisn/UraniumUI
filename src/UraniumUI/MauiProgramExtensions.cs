982a56e1b7d214a0316fa551c0e63e949321f62b
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-08 01:04:00 +0300

    Material Buttons Implementation

--- src/UraniumUI/MauiProgramExtensions.cs
+++ src/UraniumUI/MauiProgramExtensions.cs
@@ -1,10 +1,13 @@
 ﻿using InputKit.Handlers;
+using UraniumUI.Handlers;
 
 namespace UraniumUI;
 public static class MauiProgramExtensions
 {
     public static IMauiHandlersCollection AddUraniumUIHandlers(this IMauiHandlersCollection collection)
     {
-        return collection.AddInputKitHandlers();
+        return collection.AddInputKitHandlers()
+            .AddHandler(typeof(Button),typeof(StatefulButtonHandler))
+            ;
     }
 }

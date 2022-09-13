b81107bf12f431861f4926b1375266574aaf1a22
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-13 22:23:58 +0300

    prototype of textfield

--- demo/UraniumApp/MainPage.xaml.cs
+++ demo/UraniumApp/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using CommunityToolkit.Maui.Views;
+using Microsoft.Maui.Animations;
 using UraniumUI.Extensions;
 using UraniumUI.Pages;
 
@@ -6,27 +7,68 @@ namespace UraniumApp;
 
 public partial class MainPage : UraniumContentPage
 {
-	int count = 0;
+    int count = 0;
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+    }
 
-	private async void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private async void OnCounterClicked(object sender, EventArgs e)
+    {
+        count++;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if (count == 1)
+            CounterBtn.Text = $"Clicked {count} time";
+        else
+            CounterBtn.Text = $"Clicked {count} times";
 
 
-        var result = await this.DisplayTextPromptAsync("Your Name", "What is your name?", placeholder: "Uvuvwevwevwe...Osas");
+        var fullWidth = (border.Width + border.Height) * 3 / 7;
 
-        await DisplayAlert("Result:", result, "OK");
+        var rateOfLabel = title.Width / border.Width;
+
+        var finalValue = fullWidth * rateOfLabel;
+
+
+        //border.StrokeDashArray = new double[] { 0, finalValue, 500 };
+
+        await DisplayAlert("Calculated",
+            $"fullWidth: {fullWidth}\nRateOfLabel: {rateOfLabel}\nFinalValue: {finalValue}",
+            "ok");
+
+
+        //      var result = await this.DisplayTextPromptAsync("Your Name", "What is your name?", placeholder: "Uvuvwevwevwe...Osas");
+
+        //await DisplayAlert("Result:", result, "OK");
 
         SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    }
+
+    private void offsetChanged(object sender, ValueChangedEventArgs e)
+    {
+        border.StrokeDashOffset = e.NewValue;
+    }
+
+    private void entry_Focused(object sender, FocusEventArgs e)
+    {
+
+
+        title.TranslateTo(title.TranslationX, title.TranslationY - 25);
+
+        border.Stroke = Brush.Transparent;
+        borderInner.Stroke = Colors.White;
+    }
+
+    private void entry_Unfocused(object sender, FocusEventArgs e)
+    {
+        //border.StrokeDashArray[0] = 1;
+        //border.StrokeDashArray[1] = 28;
+        //border.StrokeDashArray[2] = 500;
+        //border.StrokeThickness = 1;
+
+        title.TranslateTo(title.TranslationX, title.TranslationY + 25);
+        border.Stroke = Colors.White;
+        borderInner.Stroke = Brush.Transparent;
+    }
 }
---
+++ src/UraniumUI.Material/Controls/TextField.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.Maui.Controls.Shapes;
+using UraniumUI.Resources;
+
+namespace UraniumUI.Material.Controls;
+public partial class TextField : Grid
+{
+    Entry mainEntry = new Entry
+    {
+        Margin = 1,
+    };
+    Label labelTitle = new Label()
+    {
+        Text = "Title",
+        HorizontalOptions = LayoutOptions.Start,
+        InputTransparent = true,
+        Margin = 15,
+    };
+
+    Border borderUnfocused = new Border
+    {
+        Padding = 0,
+        //Stroke = ColorResource.GetColor("Surface", "SurfaceDark", Colors.Gray),
+        StrokeThickness = 2,
+        InputTransparent = true,
+        StrokeShape = new RoundRectangle
+        {
+            CornerRadius = 8
+        }
+    };
+
+    Border borderFocused = new Border
+    {
+        Stroke = ColorResource.GetColor("Primary", "PrimaryDark", Colors.Purple),
+        InputTransparent = true,
+        StrokeThickness = 2.5,
+        IsVisible = false,
+        StrokeShape = new RoundRectangle
+        {
+            CornerRadius = 8
+        }
+    };
+
+    public TextField()
+    {
+        this.Add(borderUnfocused);
+        //this.Add(borderFocused);
+        this.Add(mainEntry);
+        this.Add(labelTitle);
+        labelTitle.Scale = 1.2;
+        //borderFocused.Content = mainEntry;
+
+        mainEntry.Focused += MainEntry_Focused;
+        mainEntry.Unfocused += MainEntry_Unfocused;
+    }
+
+    private void MainEntry_Focused(object sender, FocusEventArgs e)
+    {
+        borderFocused.IsVisible = true;
+        borderUnfocused.IsVisible = false;
+
+        labelTitle.TranslateTo(labelTitle.TranslationX, labelTitle.TranslationY - 25, 90);
+        labelTitle.TextColor = ColorResource.GetColor("Primary", "PrimaryDark", Colors.Purple);
+        labelTitle.ScaleTo(1, 90);
+    }
+
+    private void MainEntry_Unfocused(object sender, FocusEventArgs e)
+    {
+        borderFocused.IsVisible = false;
+        borderUnfocused.IsVisible = true;
+
+        labelTitle.TranslateTo(labelTitle.TranslationX, labelTitle.TranslationY + 25, 90);
+        labelTitle.TextColor = ColorResource.GetColor("OnBackground", "OnBackgroundDark", Colors.Gray);
+        labelTitle.ScaleTo(1.2, 90);
+    }
+
+    protected override SizeRequest OnMeasure(double widthConstraint, double heightConstraint)
+    {
+        //var space = (labelTitle.Width / 2) * 1.13;
+        //var perimeter = (widthConstraint + height) * 2;
+        return base.OnMeasure(widthConstraint, heightConstraint);
+    }
+
+    protected override async void OnSizeAllocated(double width, double height)
+    {
+        await Task.Delay(100);
+
+        var space = (labelTitle.Width / 2) * 1.13;
+        var perimeter = (width + height) * 2;
+
+        if (borderFocused != null)
+        {
+            this.Remove(borderFocused);
+        }
+
+        borderFocused.StrokeDashArray = new double[] { 5, space, perimeter };
+        //borderFocused.StrokeDashArray.Add(5);
+        //borderFocused.StrokeDashArray.Add(space);
+        //borderFocused.StrokeDashArray.Add(perimeter);
+
+        this.Add(borderFocused);
+
+        base.OnSizeAllocated(width, height);
+    }
+}

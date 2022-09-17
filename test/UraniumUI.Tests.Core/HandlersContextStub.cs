fccbe1d07bee928a110c51ec32783506afbaad7b
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-17 19:27:30 +0300

    Add UnitTests Infrastructure

---
+++ test/UraniumUI.Tests.Core/HandlersContextStub.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Maui.Animations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UraniumUI.Tests.Core;
+
+public class HandlersContextStub : IMauiContext
+{
+    public HandlersContextStub(IServiceProvider services)
+    {
+        Services = services;
+        Handlers = Services.GetRequiredService<IMauiHandlersFactory>();
+        AnimationManager = Services.GetRequiredService<IAnimationManager>();
+    }
+
+    public IServiceProvider Services { get; }
+
+    public IMauiHandlersFactory Handlers { get; }
+
+    public IAnimationManager AnimationManager { get; }
+}

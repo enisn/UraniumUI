0f752b1a0d2e54986417a5d12fb616903384fe2c
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-18 11:29:48 +0300

    Update TimePickerField_Test.cs

--- test/UraniumUI.Material.Tests/Controls/TimePickerField_Test.cs
+++ test/UraniumUI.Material.Tests/Controls/TimePickerField_Test.cs
@@ -1,10 +1,4 @@
-﻿using Plainer.Maui.Controls;
-using Shouldly;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Shouldly;
 using UraniumUI.Material.Controls;
 using UraniumUI.Tests.Core;
 
@@ -58,10 +52,157 @@ public class TimePickerField_Test
         viewModel.Time.ShouldBe(control.Time);
     }
 
+    [Fact]
+    public void Format_ShouldBeSet_FromViewModel()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var viewModel = new TestViewModel();
+        viewModel.Format = "HH:mm"; //24H format
+        control.BindingContext = viewModel;
+
+        // Act
+        control.SetBinding(TimePickerField.FormatProperty, new Binding(nameof(TestViewModel.Format)));
+
+        // Assert
+        control.TimePickerView.Format.ShouldBe(viewModel.Format);
+    }
+
+    [Fact]
+    public void TextColor_ShouldBeSet_FromViewModel()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var viewModel = new TestViewModel();
+        viewModel.TextColor = Colors.Blue;
+        control.BindingContext = viewModel;
+
+        // Act
+        control.SetBinding(TimePickerField.TextColorProperty, new Binding(nameof(TestViewModel.TextColor)));
+
+        // Assert
+        control.TimePickerView.TextColor.ShouldBe(viewModel.TextColor);
+    }
+
+    [Fact]
+    public void CharacterSpacing_ShouldBeSet_FromViewModel()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var viewModel = new TestViewModel();
+        viewModel.CharacterSpacing = 4.41;
+        control.BindingContext = viewModel;
+
+        // Act
+        control.SetBinding(TimePickerField.CharacterSpacingProperty, new Binding(nameof(TestViewModel.CharacterSpacing)));
+
+        // Assert
+        control.TimePickerView.CharacterSpacing.ShouldBe(viewModel.CharacterSpacing);
+    }
+
+    [Fact]
+    public void FontAttributes_ShouldBeSet_FromControl()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var fontAttributes = FontAttributes.Italic;
+
+        // Act
+        control.FontAttributes = fontAttributes;
+
+        // Assert
+        control.TimePickerView.FontAttributes.ShouldBe(fontAttributes);
+    }
+
+    [Fact]
+    public void FontAttributes_ShouldBeSet_FromViewModel()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var viewModel = new TestViewModel();
+        viewModel.FontAttributes = FontAttributes.Italic;
+        control.BindingContext = viewModel;
+
+        // Act
+        control.SetBinding(TimePickerField.FontAttributesProperty, new Binding(nameof(TestViewModel.FontAttributes)));
+
+        // Assert
+        control.TimePickerView.FontAttributes.ShouldBe(viewModel.FontAttributes);
+    }
+
+    [Fact]
+    public void FontFamily_ShouldBeSet_FromViewModel()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var viewModel = new TestViewModel();
+        viewModel.FontFamily = "Roboto";
+        control.BindingContext = viewModel;
+
+        // Act
+        control.SetBinding(TimePickerField.FontFamilyProperty, new Binding(nameof(TestViewModel.FontFamily)));
+
+        // Assert
+        control.TimePickerView.FontFamily.ShouldBe(viewModel.FontFamily);
+    }
+
+    [Fact]
+    public void FontFamily_ShouldBeSet_FromControl()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var fontFamily = "Roboto";
+
+        // Act
+        control.FontFamily = fontFamily;
+
+        // Assert
+        control.TimePickerView.FontFamily.ShouldBe(fontFamily);
+    }
+
+    [Fact]
+    public void FontSize_ShouldBeSet_FromViewModel()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var viewModel = new TestViewModel();
+        viewModel.FontSize = 28.5;
+        control.BindingContext = viewModel;
+
+        // Act
+        control.SetBinding(TimePickerField.FontSizeProperty, new Binding(nameof(TestViewModel.FontSize)));
+
+        // Assert
+        control.TimePickerView.FontSize.ShouldBe(viewModel.FontSize);
+    }
+
+    [Fact]
+    public void FontSize_ShouldBeSet_FromControl()
+    {
+        var control = AnimationReadyHandler.Prepare(new TimePickerField());
+        var fontSize = 24.75;
+
+        // Act
+        control.FontSize = fontSize;
+
+        // Assert
+        control.TimePickerView.FontSize.ShouldBe(fontSize);
+    }
+
     public class TestViewModel : UraniumBindableObject
     {
         private TimeSpan time;
+        private string format;
+        private Color textColor;
+        private double characterSpacing;
+        private FontAttributes fontAttributes;
+        private string fontFamily;
+        private double fontSize;
 
         public TimeSpan Time { get => time; set => SetProperty(ref time, value); }
+
+        public string Format { get => format; set => SetProperty(ref format, value); }
+
+        public Color TextColor { get => textColor; set => SetProperty(ref textColor, value); }
+
+        public double CharacterSpacing { get => characterSpacing; set => SetProperty(ref characterSpacing, value); }
+
+        public FontAttributes FontAttributes { get => fontAttributes; set => SetProperty(ref fontAttributes, value); }
+
+        public string FontFamily { get => fontFamily; set => SetProperty(ref fontFamily, value); }
+
+        public double FontSize { get => fontSize; set => SetProperty(ref fontSize , value); }
     }
 }

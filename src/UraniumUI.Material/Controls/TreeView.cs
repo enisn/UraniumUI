1b0860dc8193a6df49ff88504abffb45346871b5
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-09-21 20:21:59 +0300

    Selection support

--- src/UraniumUI.Material/Controls/TreeView.cs
+++ src/UraniumUI.Material/Controls/TreeView.cs
@@ -13,15 +13,89 @@ public partial class TreeView : VerticalStackLayout
         return label;
     });
 
+    public static DataTemplate CheckBoxItemTemplate = new DataTemplate(() =>
+    {
+        var checkBox = new CheckBox();
+        checkBox.SetBinding(CheckBox.TextProperty, new Binding("Name"));
+
+        checkBox.CheckChanged += (s, e) =>
+        {
+            var lastState = TreeView.GetIsSelected(checkBox);
+
+            if (lastState == null)
+            {
+                TreeView.SetIsSelected(checkBox, false);
+            }
+            else
+            {
+                TreeView.SetIsSelected(checkBox, !lastState);
+            }
+        };
+
+        VisualStateManager.SetVisualStateGroups(checkBox, new VisualStateGroupList
+        {
+            new VisualStateGroup
+            {
+                Name = "SelectionVisualStateGroup",
+                States =
+                {
+                    new VisualState
+                    {
+                        Name = "Selected",
+                        Setters =
+                        {
+                            new Setter { Property = CheckBox.IsCheckedProperty, Value = true },
+                            new Setter { Property = CheckBox.IconGeometryProperty, Value = InputKit.Shared.Controls.PredefinedShapes.Check }
+                        }
+                    },
+                    new VisualState
+                    {
+                        Name = "SemiSelected",
+                        Setters =
+                        {
+                            new Setter { Property = CheckBox.IsCheckedProperty, Value = true },
+                            new Setter { Property = CheckBox.IconGeometryProperty, Value = InputKit.Shared.Controls.PredefinedShapes.Line }
+                        }
+                    },
+                    new VisualState
+                    {
+                        Name = "UnSelected",
+                        Setters =
+                        {
+                            new Setter { Property = CheckBox.IsCheckedProperty, Value = false },
+                            new Setter { Property = CheckBox.IconGeometryProperty, Value = InputKit.Shared.Controls.PredefinedShapes.Check }
+                        }
+                    }
+                }
+            }
+        });
+
+        return checkBox;
+    });
     public TreeView()
     {
         BindableLayout.SetItemTemplate(this, new DataTemplate(() =>
         {
-            return new TreeViewNodeHolderView(ItemTemplate);
+            return new TreeViewNodeHolderView(ItemTemplate, IsExpandedPropertyName);
         }));
     }
 
     public BindingBase ChildrenBinding { get; set; } = new Binding("Children");
+
+    private string isExpandedBinding;
+
+    public string IsExpandedPropertyName
+    {
+        get => isExpandedBinding;
+        set
+        {
+            isExpandedBinding = value;
+            foreach (TreeViewNodeHolderView view in this.Children.Where(x => x is TreeViewNodeHolderView))
+            {
+                view.IsTextendedProperty = value;
+            }
+        }
+    }
 
     protected virtual void OnItemsSourceSet()
     {
@@ -30,7 +104,10 @@ public partial class TreeView : VerticalStackLayout
 
     private void OnItemTemplateChanged()
     {
-
+        BindableLayout.SetItemTemplate(this, new DataTemplate(() =>
+        {
+            return new TreeViewNodeHolderView(ItemTemplate, IsExpandedPropertyName);
+        }));
     }
 
     public IList ItemsSource { get => (IList)GetValue(ItemsSourceProperty); set => SetValue(ItemsSourceProperty, value); }
@@ -43,7 +120,7 @@ public partial class TreeView : VerticalStackLayout
 
     public static readonly BindableProperty ItemTemplateProperty = BindableProperty.Create(
         nameof(ItemTemplate), typeof(DataTemplate), typeof(TreeView),
-        defaultValue: DefaultItemTemplate, propertyChanged: (b, o, n) => (b as TreeView).OnItemTemplateChanged());
+        defaultValue: CheckBoxItemTemplate, propertyChanged: (b, o, n) => (b as TreeView).OnItemTemplateChanged());
 
     public static readonly BindableProperty IsExpandedProperty =
         BindableProperty.CreateAttached("IsExpanded", typeof(bool), typeof(TreeViewNodeHolderView), false,
@@ -52,4 +129,13 @@ public partial class TreeView : VerticalStackLayout
     public static bool GetIsExpanded(BindableObject view) => (bool)view.GetValue(IsExpandedProperty);
 
     public static void SetIsExpanded(BindableObject view, bool value) => view.SetValue(IsExpandedProperty, value);
+
+
+    public static readonly BindableProperty IsSelectedProperty =
+        BindableProperty.CreateAttached("IsSelected", typeof(bool?), typeof(View), false,
+            propertyChanged: (bindable, oldValue, newValue) => ((bindable as View).Parent.Parent.Parent as TreeViewNodeHolderView).OnIsSelectedChanged());
+
+    public static bool? GetIsSelected(BindableObject view) => (bool?)view.GetValue(IsSelectedProperty);
+
+    public static void SetIsSelected(BindableObject view, bool? value) => view.SetValue(IsSelectedProperty, value);
 }
--- src/UraniumUI.Material/Controls/TreeViewNodeHolderView.cs
+++ src/UraniumUI.Material/Controls/TreeViewNodeHolderView.cs
@@ -1,16 +1,15 @@
-﻿using Microsoft.Maui.Controls;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using InputKit.Shared.Controls;
 using UraniumUI.Pages;
 using UraniumUI.Resources;
+using UraniumUI.Triggers;
 using Path = Microsoft.Maui.Controls.Shapes.Path;
 
 namespace UraniumUI.Material.Controls;
 public class TreeViewNodeHolderView : VerticalStackLayout
 {
     public View NodeView { get => nodeContainer.Content; set => nodeContainer.Content = value; }
+
+    public TreeViewNodeHolderView Parent { get; private set; }
 
     protected ContentView nodeContainer = new ContentView
     {
@@ -35,15 +34,46 @@ public class TreeViewNodeHolderView : VerticalStackLayout
         IsVisible = false
     };
 
-    public DataTemplate DataTemplate { get; private set; }
+    public DataTemplate DataTemplate { get; }
 
-    public TreeViewNodeHolderView(DataTemplate dataTemplate)
+    public bool IsLeaf => !stackLayoutChildren.Children.Any();
+
+    private string isTextendedProperty;
+
+    public string IsTextendedProperty
+    {
+        get => isTextendedProperty;
+        set
+        {
+            isTextendedProperty = value;
+            this.SetBinding(TreeView.IsExpandedProperty, new Binding(value, BindingMode.TwoWay));
+            foreach (TreeViewNodeHolderView item in this.Children.Where(x => x is TreeViewNodeHolderView))
+            {
+                item.IsTextendedProperty = value;
+            }
+        }
+    }
+
+    public string isSelectedProperty;
+
+    public string IsSelectedProperty
+    {
+        get => isTextendedProperty;
+        set
+        {
+            isSelectedProperty = value;
+            this.SetBinding(TreeView.IsSelectedProperty, new Binding(value, BindingMode.TwoWay));
+            foreach (TreeViewNodeHolderView item in this.Children.Where(x => x is TreeViewNodeHolderView))
+            {
+                item.IsTextendedProperty = value;
+            }
+        }
+    }
+
+    public TreeViewNodeHolderView(DataTemplate dataTemplate, string isTextendedProperty)
     {
         DataTemplate = dataTemplate;
-
-        //TODO: Experimental / Remove Later
         nodeContainer.Content = DataTemplate.CreateContent() as View;
-        // Experimental
 
         this.Add(new HorizontalStackLayout
         {
@@ -55,16 +85,44 @@ public class TreeViewNodeHolderView : VerticalStackLayout
         });
         this.Add(stackLayoutChildren);
 
-        this.SetBinding(TreeView.IsExpandedProperty, new Binding("IsExtended", BindingMode.TwoWay));
+        this.SetBinding(TreeView.IsExpandedProperty, new Binding(isTextendedProperty, BindingMode.TwoWay));
+        this.SetBinding(TreeView.IsSelectedProperty, new Binding("IsSelected", BindingMode.TwoWay));
 
         BindableLayout.SetItemTemplate(stackLayoutChildren, new DataTemplate(() =>
         {
-            return new TreeViewNodeHolderView(DataTemplate);
+            var node = new TreeViewNodeHolderView(DataTemplate, isTextendedProperty);
+
+            node.Parent = this;
+
+            return node;
         }));
 
         stackLayoutChildren.SetBinding(BindableLayout.ItemsSourceProperty, new Binding("Children"));
 
+        stackLayoutChildren.ChildAdded += (s, e) => OnPropertyChanged(nameof(IsLeaf));
+        stackLayoutChildren.ChildRemoved += (s, e) => OnPropertyChanged(nameof(IsLeaf));
+
         InitializeArrowButton();
+
+        NodeView.Triggers.Add(new DataTrigger(typeof(View))
+        {
+            Binding = new Binding("IsSelected", source: this),
+            Value = true,
+            EnterActions =
+            {
+                new GenericTriggerAction<View>((_) =>
+                {
+                    OnIsSelectedChanged();
+                })
+            },
+            ExitActions =
+            {
+                new GenericTriggerAction<View>((_) =>
+                {
+                    OnIsSelectedChanged();
+                })
+            }
+        });
     }
 
     private void InitializeArrowButton()
@@ -75,10 +133,33 @@ public class TreeViewNodeHolderView : VerticalStackLayout
         {
             TreeView.SetIsExpanded(this, !TreeView.GetIsExpanded(this));
         };
+
+        iconArrow.Triggers.Add(new DataTrigger(typeof(ContentView))
+        {
+            Binding = new Binding(nameof(IsLeaf), source: this),
+            Value = true,
+            EnterActions =
+            {
+                new GenericTriggerAction<ContentView>((view) =>
+                {
+                    view.Opacity = 0;
+                    view.InputTransparent = true;
+                })
+            },
+            ExitActions =
+            {
+                new GenericTriggerAction<ContentView>((view) =>
+                {
+                    view.Opacity = 1;
+                    view.InputTransparent = false;
+                })
+            }
+        });
     }
 
     protected internal virtual void OnIsExpandedChanged()
     {
+
         if (TreeView.GetIsExpanded(this))
         {
             iconArrow.RotateTo(90);
@@ -89,12 +170,45 @@ public class TreeViewNodeHolderView : VerticalStackLayout
             iconArrow.RotateTo(0);
             stackLayoutChildren.IsVisible = false;
         }
+    }
 
-        Button btn = new Button();
+    protected internal virtual void OnIsSelectedChanged()
+    {
+        var isSelected = TreeView.GetIsSelected(NodeView);
 
-        OnPropertyChanged("IsExpanded");
-        OnPropertyChanged("IsExtended");
+        VisualStateManager.GoToState(NodeView, isSelected == null ? "SemiSelected" : isSelected == true ? "Selected" : "UnSelected");
 
+        if (IsLeaf)
+        {
+            Parent?.OnChildSelectionChanged();
+            return;
+        }
+    }
 
+    protected void OnChildSelectionChanged()
+    {
+        var query = stackLayoutChildren.Children.OfType<TreeViewNodeHolderView>();
+
+        var first = query.FirstOrDefault(x => TreeView.GetIsSelected(x) != null);
+
+        if (first == null)
+        {
+            TreeView.SetIsSelected(NodeView, null);
+        }
+
+        var firstIsSelected = TreeView.GetIsSelected(first);
+
+        foreach (var item in query)
+        {
+            if (firstIsSelected != TreeView.GetIsSelected(item))
+            {
+                TreeView.SetIsSelected(NodeView, null);
+                break;
+            }
+        }
+
+        TreeView.SetIsSelected(NodeView, firstIsSelected);
+
+        Parent?.OnChildSelectionChanged();
     }
 }

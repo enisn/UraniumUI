521bd34f8865643c9bad9c3e8e1822687e8cb919
enisn <iletisim@enisnecipoglu.com>
2022-11-19 16:46:21 +0300

    Add bindable propertyies for StatefulContentView

--- src/UraniumUI/Views/StatefulContentView.cs
+++ src/UraniumUI/Views/StatefulContentView.cs
@@ -1,27 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Input;
+﻿using System.Windows.Input;
 
 namespace UraniumUI.Views;
 public class StatefulContentView : ContentView
 {
-	public StatefulContentView()
-	{
-	}
+    public ICommand PressedCommand { get => (ICommand)GetValue(PressedCommandProperty); set => SetValue(PressedCommandProperty, value); }
 
-    // TODO: Convert them to BindableProperty
-	public ICommand PressedCommand { get; set; }
+    public static BindableProperty PressedCommandProperty = BindableProperty.Create(nameof(PressedCommand), typeof(ICommand), typeof(StatefulContentView));
 
-	public ICommand HoverCommand { get; set; }
+    public ICommand HoverCommand { get => (ICommand)GetValue(HoverCommandProperty); set => SetValue(HoverCommandProperty, value); }
 
-	public ICommand HoverExitCommand { get; set; }
+    public static BindableProperty HoverCommandProperty = BindableProperty.Create(nameof(HoverCommand), typeof(ICommand), typeof(StatefulContentView));
 
-	public ICommand LongPressCommand { get; set; }
+    public ICommand HoverExitCommand { get => (ICommand)GetValue(HoverExitCommandProperty); set => SetValue(HoverExitCommandProperty, value); }
 
-	public ICommand TappedCommand { get; set; }
+    public static BindableProperty HoverExitCommandProperty = BindableProperty.Create(nameof(HoverExitCommand), typeof(ICommand), typeof(StatefulContentView));
 
-	public object CommandParameter { get; set; }
+    public ICommand LongPressCommand { get => (ICommand)GetValue(LongPressCommandProperty); set => SetValue(LongPressCommandProperty, value); }
+
+    public static BindableProperty LongPressCommandProperty = BindableProperty.Create(nameof(LongPressCommand), typeof(ICommand), typeof(StatefulContentView));
+
+    public ICommand TappedCommand { get => (ICommand)GetValue(TappedCommandProperty); set => SetValue(TappedCommandProperty, value); }
+
+    public static BindableProperty TappedCommandProperty = BindableProperty.Create(nameof(TappedCommand), typeof(ICommand), typeof(StatefulContentView));
+
+    public object CommandParameter { get => GetValue(CommandParameterProperty); set => SetValue(CommandParameterProperty, value); }
+
+    public static BindableProperty CommandParameterProperty = BindableProperty.Create(nameof(CommandParameter), typeof(object), typeof(StatefulContentView));
 }

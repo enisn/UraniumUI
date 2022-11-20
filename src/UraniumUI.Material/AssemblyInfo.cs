474dfa38f53de41aa450efec6c09ac1f0de457ad
enisn <iletisim@enisnecipoglu.com>
2022-11-20 17:20:07 +0300

    Define xml namespaces

---
+++ src/UraniumUI.Material/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.NamespacePrefix + nameof(UraniumUI.Material.Attachments))]
+[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.NamespacePrefix + nameof(UraniumUI.Material.Controls))]
+[assembly: XmlnsDefinition(Constants.XamlNamespace, Constants.NamespacePrefix + nameof(UraniumUI.Material.Resources))]
+
+[assembly: Microsoft.Maui.Controls.XmlnsPrefix(Constants.XamlNamespace, "material")]
+
+
+class Constants
+{
+    public const string XamlNamespace = "http://schemas.microsoft.com/dotnet/2022/maui/uraniumui/material";
+
+    public const string NamespacePrefix = $"{nameof(UraniumUI)}.{nameof(UraniumUI.Material)}.";
+}

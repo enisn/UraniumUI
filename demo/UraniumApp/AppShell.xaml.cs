97fe05336d464618e0704cf50ae1c526db10c17c
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-11-20 16:34:39 +0300

    Add theme switch to FlyoutHeader

--- demo/UraniumApp/AppShell.xaml.cs
+++ demo/UraniumApp/AppShell.xaml.cs
@@ -2,8 +2,18 @@
 
 public partial class AppShell : Shell
 {
-	public AppShell()
-	{
-		InitializeComponent();
-	}
+    public AppShell()
+    {
+        InitializeComponent();
+        themeSwitch.IsToggled = App.Current.RequestedTheme == AppTheme.Dark;
+        App.Current.RequestedThemeChanged += (s, e) =>
+        {
+            themeSwitch.IsToggled = App.Current.RequestedTheme == AppTheme.Dark;
+        };
+    }
+
+    private void ThemeToggled(object sender, ToggledEventArgs e)
+    {
+        App.Current.UserAppTheme = e.Value ? AppTheme.Dark : AppTheme.Light;
+    }
 }

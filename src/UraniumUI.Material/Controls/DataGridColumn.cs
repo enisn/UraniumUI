47b019fd4f4eb638320b766b15b48bbcdbd954bf
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-12-21 18:42:41 +0300

    Add info for Obsolete attribute

--- src/UraniumUI.Material/Controls/DataGridColumn.cs
+++ src/UraniumUI.Material/Controls/DataGridColumn.cs
@@ -10,7 +10,7 @@ public class DataGridColumn
 
     public DataTemplate CellItemTemplate { get; set; }
 
-    [Obsolete]
+    [Obsolete("Use Binding property instead. Replace 'PropertyName=\"Name\"' with 'Binding=\"{Binding Name}\"'. Visit https://github.com/enisn/UraniumUI/blob/develop/docs/en/themes/material/components/DataGrid.md for the usage.")]
     public string PropertyName { get; set; }
 
     public BindingBase Binding { get; set; }

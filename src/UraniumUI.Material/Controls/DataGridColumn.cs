e92743bdac159dfc5f23d5ebed2077055b3bbe33
Enis Necipoglu <iletisim@enisnecipoglu.com>
2022-12-21 09:52:34 +0300

    Remove CellBindingContext and use bindings in datagrid cells

--- src/UraniumUI.Material/Controls/DataGridColumn.cs
+++ src/UraniumUI.Material/Controls/DataGridColumn.cs
@@ -10,10 +10,14 @@ public class DataGridColumn
 
     public DataTemplate CellItemTemplate { get; set; }
 
+    [Obsolete]
     public string PropertyName { get; set; }
+
+    public BindingBase Binding { get; set; }
 
     [TypeConverter(typeof(GridLengthTypeConverter))]
     public GridLength Width { get; set; } = GridLength.Auto;
 
+    [Obsolete]
     internal PropertyInfo PropertyInfo { get; set; }
 }

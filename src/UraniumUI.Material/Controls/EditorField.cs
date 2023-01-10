2d0cb525d614184df845e440f833fbb8727d79dc
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-01-10 22:40:59 +0300

    Add EditorField

---
+++ src/UraniumUI.Material/Controls/EditorField.cs
@@ -0,0 +1,21 @@
+﻿using Plainer.Maui.Controls;
+
+namespace UraniumUI.Material.Controls;
+public class EditorField : InputField
+{
+    public EditorView EditorView => Content as EditorView;
+    
+    public override View Content { get; set; } = new EditorView
+    {
+        Margin = new Thickness(10, 0),
+        BackgroundColor = Colors.Transparent,
+        VerticalOptions = LayoutOptions.Center,
+        AutoSize = EditorAutoSizeOption.TextChanges,
+    };
+    public override bool HasValue { get => !string.IsNullOrEmpty(EditorView?.Text); }
+
+    protected override object GetValueForValidator()
+    {
+        return EditorView.Text;
+    }
+}

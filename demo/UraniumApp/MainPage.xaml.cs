3439032b48f61554be0dee4e6dc6728709c67465
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-01-12 01:12:15 +0300

    Fix code-factor feedbacks

--- demo/UraniumApp/MainPage.xaml.cs
+++ demo/UraniumApp/MainPage.xaml.cs
@@ -8,18 +8,4 @@ public partial class MainPage : UraniumContentPage
     {
         InitializeComponent();
     }
-    
-    
-
-    //private async void OnCounterClicked(object sender, EventArgs e)
-    //{
-    //    count++;
-
-    //    if (count == 1)
-    //        CounterBtn.Text = $"Clicked {count} time";
-    //    else
-    //        CounterBtn.Text = $"Clicked {count} times";
-
-    //    SemanticScreenReader.Announce(CounterBtn.Text);
-    //}
 }

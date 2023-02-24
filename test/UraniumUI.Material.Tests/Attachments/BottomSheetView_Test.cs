cb9ad92d7a60c410c08e8d44f5d27cdda3ae78cf
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-02-25 01:22:56 +0300

    Add unit tests to validate #243

---
+++ test/UraniumUI.Material.Tests/Attachments/BottomSheetView_Test.cs
@@ -0,0 +1,45 @@
+﻿using Shouldly;
+using UraniumUI.Material.Attachments;
+using UraniumUI.Tests.Core;
+
+namespace UraniumUI.Material.Tests.Attachments;
+public class BottomSheetView_Test
+{
+    public BottomSheetView_Test()
+    {
+        ApplicationExtensions.CreateAndSetMockApplication();
+    }
+
+    [Fact]
+    public void DisablePageWhenOpened_BindingForInitialization_FromSource()
+    {
+        var control = AnimationReadyHandler.Prepare(new BottomSheetView());
+        var viewModel = new { DisablePageWhenOpened = false };
+        control.BindingContext = viewModel;
+        control.SetBinding(BottomSheetView.DisablePageWhenOpenedProperty, new Binding(nameof(viewModel.DisablePageWhenOpened)));
+
+        // Assert
+        control.DisablePageWhenOpened.ShouldBe(viewModel.DisablePageWhenOpened);
+    }
+
+    [Fact]
+    public void DisablePageWhenOpened_ShouldBeChanged_FromSource()
+    {
+        var control = AnimationReadyHandler.Prepare(new BottomSheetView());
+        var viewModel = new BottomSheetTestViewModel { DisablePageWhenOpened = false };
+        control.BindingContext = viewModel;
+        control.SetBinding(BottomSheetView.DisablePageWhenOpenedProperty, new Binding(nameof(viewModel.DisablePageWhenOpened)));
+
+        // Act
+        viewModel.DisablePageWhenOpened = true;
+
+        // Assert
+        control.DisablePageWhenOpened.ShouldBe(viewModel.DisablePageWhenOpened);
+    }
+
+    internal class BottomSheetTestViewModel : UraniumBindableObject
+    {
+        private bool disablePageWhenOpened;
+        public bool DisablePageWhenOpened { get => disablePageWhenOpened; set => SetProperty(ref disablePageWhenOpened, value); }
+    }
+}
---
+++ test/UraniumUI.Material.Tests/Controls/AutoCompleteTextField_Test.cs
@@ -0,0 +1,61 @@
+﻿using Shouldly;
+using UraniumUI.Material.Controls;
+using UraniumUI.Tests.Core;
+
+namespace UraniumUI.Material.Tests.Controls;
+public class AutoCompleteTextField_Test
+{
+    public AutoCompleteTextField_Test()
+    {
+        ApplicationExtensions.CreateAndSetMockApplication();
+    }
+
+    [Fact]
+    public void Text_BindingForInitialization_FromSource()
+    {
+        var control = AnimationReadyHandler.Prepare(new AutoCompleteTextField());
+        var viewModel = new { Text = "My Title" };
+        control.BindingContext = viewModel;
+        control.SetBinding(AutoCompleteTextField.TextProperty, new Binding(nameof(viewModel.Text)));
+
+        // Assert
+        control.Text.ShouldBe(viewModel.Text);
+    }
+
+    [Fact]
+    public void Text_ShouldBeChanged_FromSource()
+    {
+        var control = AnimationReadyHandler.Prepare(new AutoCompleteTextField());
+        var viewModel = new AutoCompleteTextFieldTestViewModel { Text = "My Title" };
+        control.BindingContext = viewModel;
+        control.SetBinding(AutoCompleteTextField.TextProperty, new Binding(nameof(viewModel.Text)));
+
+        // Act
+        viewModel.Text = "Title (changed)";
+
+        // Assert
+        control.Text.ShouldBe(viewModel.Text);
+    }
+
+    [Fact]
+    public void Text_ShouldBeChanged_FromControl()
+    {
+        var control = AnimationReadyHandler.Prepare(new AutoCompleteTextField());
+        var viewModel = new AutoCompleteTextFieldTestViewModel { Text = "My Title" };
+        control.BindingContext = viewModel;
+        control.SetBinding(AutoCompleteTextField.TextProperty, new Binding(nameof(viewModel.Text)));
+
+        // Act
+        control.Text = "Title (changed)";
+
+        // Assert
+        viewModel.Text.ShouldBe(control.Text);
+    }
+
+    internal class AutoCompleteTextFieldTestViewModel : UraniumBindableObject
+    {
+        private string text;
+
+        public string Text { get => text; set => SetProperty(ref text, value); }
+    }
+}

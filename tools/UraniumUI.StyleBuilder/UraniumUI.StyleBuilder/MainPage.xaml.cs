4bcddcff3fc0915c1bf1c9ff4f5ecba14c120f82
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-04-12 15:29:19 +0300

    Add StyleBuilder as a new app

---
+++ tools/UraniumUI.StyleBuilder/UraniumUI.StyleBuilder/MainPage.xaml.cs
@@ -0,0 +1,11 @@
+﻿using InputKit.Shared.Controls;
+using UraniumUI.Pages;
+
+namespace UraniumUI.StyleBuilder;
+public partial class MainPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
---
+++ tools/UraniumUI.StyleBuilder/UraniumUI.StyleBuilder/MauiProgram.cs
@@ -0,0 +1,27 @@
+﻿using InputKit.Shared.Controls;
+using Mopups.Hosting;
+using UraniumUI;
+
+namespace UraniumUI.StyleBuilder;
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureMopups()
+            .UseUraniumUI()
+            .UseUraniumUIMaterial()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                fonts.AddMaterialIconFonts();
+            });
+
+        builder.Services.AddMopupsDialogs();
+        return builder.Build();
+    }
+}

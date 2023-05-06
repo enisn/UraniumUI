df0ec15ee586b3d03d5e3affd6b6608f3431910b
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-06 22:22:37 +0300

    Apply same InputField logic to iOS & MacCatalyst

--- src/UraniumUI.Material/Controls/InputField.cs
+++ src/UraniumUI.Material/Controls/InputField.cs
@@ -46,9 +46,7 @@ public partial class InputField : Grid
 
     protected Grid rootGrid = new Grid
     {
-#if ANDROID || WINDOWS
         HeightRequest = 45 // TODO: Remove this after .NET 8. This is a workaround for https://github.com/dotnet/maui/issues/14645
-#endif
     };
 
     protected Lazy<Image> imageIcon = new Lazy<Image>(() =>

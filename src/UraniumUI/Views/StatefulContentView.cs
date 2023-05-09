4e2e180a0a039f74bc1ce6aed167ab0a4991411b
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-09 19:51:46 +0300

    Add missing events to StatefulContentView

--- src/UraniumUI/Views/StatefulContentView.cs
+++ src/UraniumUI/Views/StatefulContentView.cs
@@ -3,6 +3,22 @@
 namespace UraniumUI.Views;
 public class StatefulContentView : ContentView, IStatefulView
 {
+    public event EventHandler<EventArgs> Pressed;
+    public event EventHandler<EventArgs> LongPressed;
+    public event EventHandler<EventArgs> Hovered;
+    public event EventHandler<EventArgs> HoverExited;
+    public event EventHandler<EventArgs> Tapped;
+
+    internal void InvokePressed() => Pressed?.Invoke(this, EventArgs.Empty);
+
+    internal void InvokeLongPressed() => LongPressed?.Invoke(this, EventArgs.Empty);
+
+    internal void InvokeHovered() => Hovered?.Invoke(this, EventArgs.Empty);
+
+    internal void InvokeHoverExited() => HoverExited?.Invoke(this, EventArgs.Empty);
+
+    internal void InvokeTapped() => Tapped?.Invoke(this, EventArgs.Empty);
+
     public ICommand PressedCommand { get => (ICommand)GetValue(PressedCommandProperty); set => SetValue(PressedCommandProperty, value); }
 
     public static BindableProperty PressedCommandProperty = BindableProperty.Create(nameof(PressedCommand), typeof(ICommand), typeof(StatefulContentView));

1020212da2ca4455aad79fc18e90c9ae0606c949
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-20 23:23:43 +0300

    Make property editing automatic

--- demo/UraniumApp/ViewModels/InputFields/TextFieldViewModel.cs
+++ demo/UraniumApp/ViewModels/InputFields/TextFieldViewModel.cs
@@ -1,60 +1,28 @@
 ﻿using DotNurse.Injector.Attributes;
-using ReactiveUI;
-using ReactiveUI.Fody.Helpers;
-using System.Windows.Input;
-using System.Xml.Linq;
 using UraniumApp.Inputs.ColorPicking;
+using UraniumUI.Material.Controls;
 using UraniumUI.Resources;
 
 namespace UraniumApp.ViewModels.InputFields;
 
 [RegisterAs(typeof(TextFieldViewModel))]
-public class TextFieldViewModel : InputFieldViewModel
+public class TextFieldViewModel : SingleControlEditingViewModel<TextField>
 {
-    public string XamlSourceCode => SourceCode.ToString();
-
-    protected XDocument SourceCode { get; }
-
-    [Reactive] public string Text { get; set; }
-
-    [Reactive] public string Title { get; set; } = "Title";
-
-    protected Color defaultTextColor = ColorResource.GetColor("OnBackground", "OnBackgroundDark", Colors.Gray);
-    [Reactive] public Color TextColor { get; set; }
-
-    public ICommand PickTextColorCommand { get; }
-
-    public TextFieldViewModel(IColorPicker colorPicker) : base(colorPicker)
+    protected override TextField InitializeControl()
     {
-        SourceCode = XDocument.Parse($"""<ContentPage xmlns:material="http://schemas.enisn-projects.io/dotnet/maui/uraniumui/material"><material:TextField/></ContentPage>""");
-
-        TextColor = defaultTextColor;
-
-        PickTextColorCommand = new Command(async _ =>
-            await ColorPicker.PickCollorForAsync(this, nameof(TextColor)));
-
-        this.WhenAnyValue(x => x.Text).Subscribe(GenerateSourceCode);
-        this.WhenAnyValue(x => x.Title).Subscribe(GenerateSourceCode);
-        this.WhenAnyValue(x => x.TextColor).Subscribe(GenerateSourceCode);
-        this.WhenAnyValue(x => x.AccentColor).Subscribe(GenerateSourceCode);
-
-        GenerateSourceCode();
+        return new TextField
+        {
+            Title = "Title"
+        };
     }
 
-    protected void GenerateSourceCode(object parameter = null)
+    protected override string InitialXDocumentCode => """<ContentPage xmlns:material="http://schemas.enisn-projects.io/dotnet/maui/uraniumui/material"><material:TextField/></ContentPage>""";
+
+    protected override void InitializeDefaultValues(Dictionary<string, object> values)
     {
-        var contentPage = SourceCode.Descendants().First();
-
-        var material = contentPage.GetNamespaceOfPrefix("material");
-
-        var textField = contentPage.Descendants(material + "TextField").First();
-
-        textField.SetAttributeValue(nameof(Text), Text);
-        textField.SetAttributeValue(nameof(Title), Title);
-
-        textField.SetAttributeValue(nameof(TextColor), TextColor == defaultTextColor ? null : TextColor?.ToArgbHex());
-        textField.SetAttributeValue(nameof(AccentColor), AccentColor == defaultAccentColor ? null : AccentColor?.ToArgbHex());
-
-        this.RaisePropertyChanged(nameof(XamlSourceCode));
+        values.Add(nameof(TextField.TextColor), ColorResource.GetColor("OnBackground", "OnBackgroundDark"));
+        values.Add(nameof(TextField.AccentColor), ColorResource.GetColor("Primary", "PrimaryDark"));
+        values.Add(nameof(TextField.BorderColor), ColorResource.GetColor("OnBackground", "OnBackgroundDark"));
+        values.Add(nameof(TextField.TitleColor), ColorResource.GetColor("OnBackground", "OnBackgroundDark"));
     }
 }

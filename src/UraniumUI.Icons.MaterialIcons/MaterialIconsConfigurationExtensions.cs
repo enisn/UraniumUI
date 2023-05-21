f8e130125d34f6d177f4851358929a6849747134
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-21 23:32:38 +0300

    Update MaterialIconsConfigurationExtensions.cs

--- src/UraniumUI.Icons.MaterialIcons/MaterialIconsConfigurationExtensions.cs
+++ src/UraniumUI.Icons.MaterialIcons/MaterialIconsConfigurationExtensions.cs
@@ -1,8 +1,4 @@
-﻿#if WINDOWS
-using Microsoft.UI.Xaml.Controls;
-#endif
-
-namespace UraniumUI;
+﻿namespace UraniumUI;
 
 public static class MaterialIconsConfigurationExtensions
 {
@@ -15,10 +11,6 @@ public static class MaterialIconsConfigurationExtensions
         fonts.AddEmbeddedResourceFont(thisAssembly, "MaterialIconsRound-Regular.otf", "MaterialRound");
         fonts.AddEmbeddedResourceFont(thisAssembly, "MaterialIconsSharp-Regular.otf", "MaterialSharp");
         fonts.AddEmbeddedResourceFont(thisAssembly, "MaterialIconsTwoTone-Regular.otf", "MaterialTwoTone");
-
-#if WINDOWS
-        //Symbol.Audio
-#endif
 
         return fonts;
     }

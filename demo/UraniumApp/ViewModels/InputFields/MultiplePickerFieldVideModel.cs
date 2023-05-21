8439312c339dbf625dcc8cb7d607b0b7a54135b6
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-21 21:26:19 +0300

    Update TimePickerField in demo app

--- demo/UraniumApp/ViewModels/InputFields/MultiplePickerFieldVideModel.cs
+++ demo/UraniumApp/ViewModels/InputFields/MultiplePickerFieldVideModel.cs
@@ -1,10 +1,5 @@
 ﻿using DotNurse.Injector.Attributes;
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 using UraniumUI.Material.Controls;
 using UraniumUI.Resources;
---
+++ demo/UraniumApp/ViewModels/InputFields/TimePikerFieldViewModel.cs
@@ -0,0 +1,30 @@
+﻿using DotNurse.Injector.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UraniumUI.Material.Controls;
+using UraniumUI.Resources;
+
+namespace UraniumApp.ViewModels.InputFields;
+
+[RegisterAs(typeof(TimePikerFieldViewModel))]
+public class TimePikerFieldViewModel : SingleControlEditingViewModel<TimePickerField>
+{
+    protected override TimePickerField InitializeControl()
+    {
+        return new TimePickerField
+        {
+            Title = "Pick a time"
+        };
+    }
+
+    protected override void InitializeDefaultValues(Dictionary<string, object> values)
+    {
+        values.Add(nameof(TextField.TextColor), ColorResource.GetColor("OnBackground", "OnBackgroundDark"));
+        values.Add(nameof(TextField.AccentColor), ColorResource.GetColor("Primary", "PrimaryDark"));
+        values.Add(nameof(TextField.BorderColor), ColorResource.GetColor("OnBackground", "OnBackgroundDark"));
+        values.Add(nameof(TextField.TitleColor), ColorResource.GetColor("OnBackground", "OnBackgroundDark"));
+    }
+}

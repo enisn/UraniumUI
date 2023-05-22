6caf25744751a5f6e44d5f865edec2bc7b7c1322
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-22 12:04:59 +0300

    PoC of SelectableLabel

---
+++ src/UraniumUI/Controls/SelectableLabel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UraniumUI.Controls;
+public class SelectableLabel : Label
+{
+
+    public SelectableLabel()
+    {
+        
+    }
+}

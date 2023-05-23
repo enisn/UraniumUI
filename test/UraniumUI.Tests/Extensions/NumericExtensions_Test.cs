ee6a19d85c9e3a0708aacb605f14aadfa03821f1
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-23 22:35:55 +0300

    Update Clamp extension

---
+++ test/UraniumUI.Tests/Extensions/NumericExtensions_Test.cs
@@ -0,0 +1,40 @@
+﻿using Shouldly;
+using UraniumUI.Extensions;
+
+namespace UraniumUI.Tests.Extensions;
+public class NumericExtensions_Test
+{
+    [Fact]
+    public void Clamp_ShouldReturnSameNumber()
+    {
+        var number = 15;
+
+        var clamped = number.Clamp(10, 25);
+
+        clamped.ShouldBe(15);
+    }
+
+    [Fact]
+    public void Clamp_ShouldReturnMin()
+    {
+
+        var number = 15;
+        var min = 20;
+
+        var clamped = number.Clamp(min, 25);
+
+        clamped.ShouldBe(min);
+    }
+
+    [Fact]
+    public void Clamp_ShouldReturnMax()
+    {
+
+        var number = 15;
+        var max = 10;
+
+        var clamped = number.Clamp(0, max);
+
+        clamped.ShouldBe(max);
+    }
+}

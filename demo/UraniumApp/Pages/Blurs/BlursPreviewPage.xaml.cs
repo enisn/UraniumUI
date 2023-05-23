7851efa706dcb14cafe2632a91b7a6d9019031f5
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-05-23 20:00:43 +0300

    Update blur demo

--- demo/UraniumApp/Pages/Blurs/BlursPreviewPage.xaml.cs
+++ demo/UraniumApp/Pages/Blurs/BlursPreviewPage.xaml.cs
@@ -5,12 +5,12 @@ public partial class BlursPreviewPage : ContentPage
 	public BlursPreviewPage()
 	{
 		InitializeComponent();
-
-		img.Source = "https://images.unsplash.com/photo-1470058869958-2a77ade41c02?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80";
     }
 
-    private void rb1_SelectedItemChanged(object sender, EventArgs e)
+    protected override void OnAppearing()
     {
-        blur1.Mode = (UraniumUI.Blurs.BlurMode)rb1.SelectedIndex;
+        base.OnAppearing();
+
+        collectionView.SelectedItem = collectionView.ItemsSource.Cast<object>().LastOrDefault();
     }
 }

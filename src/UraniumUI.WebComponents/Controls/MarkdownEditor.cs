41c17a1a7103b3c86ad183e42f8eef932ae81ee6
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-10-26 13:48:30 +0300

    Add markdown editor

---
+++ src/UraniumUI.WebComponents/Controls/MarkdownEditor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UraniumUI.WebComponents.Controls;
+public class MarkdownEditor : WebView, IMarkdownEditor
+{
+    public MarkdownEditor()
+    {
+        BackgroundColor = Colors.Transparent;
+        Source = (WebViewSource)new WebViewSourceTypeConverter().ConvertFrom("markdowneditor.html");
+    }
+
+    public string Content { get; set; }
+}

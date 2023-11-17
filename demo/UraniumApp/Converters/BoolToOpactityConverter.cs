ea6662736ef97d379d25fc307aab953233a8f376
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-11-17 18:42:27 +0300

    Enhance TreeView demo page

---
+++ demo/UraniumApp/Converters/BoolToOpactityConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+
+namespace UraniumApp.Converters;
+public class BoolToOpactityConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return b ? 1 : 0;
+        }
+
+        return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is bool b)
+        {
+            return b ? 1 : 0;
+        }
+
+        return value;
+    }
+}
---
+++ demo/UraniumApp/UraniumConverters.cs
@@ -0,0 +1,9 @@
+﻿using UraniumApp.Converters;
+
+namespace UraniumApp;
+public static class UraniumConverters
+{
+    public static BooleanInverter BooleanInverter { get; } = new();
+
+    public static BoolToOpactityConverter BoolToOpactityConverter { get; } = new();
+}

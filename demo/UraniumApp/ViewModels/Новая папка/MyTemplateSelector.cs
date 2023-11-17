911c2356a53ac8a307a3d66cde6a70a180e3c68d
Кожевников Олег Евгеньевич <koe@kombinat.ru>
2023-11-17 16:03:04 +0500

    Add using DataTemplateSelector with TreeView

---
+++ demo/UraniumApp/ViewModels/Новая папка/MyTemplateSelector.cs
@@ -0,0 +1,30 @@
+﻿namespace UraniumApp.ViewModels;
+
+public class MyTemplateSelector : DataTemplateSelector
+{
+    public DataTemplate TemplateDefault { get; set; } = DefaultItemTemplate;
+    public DataTemplate TemplateA { get; set; } = DefaultItemTemplate;
+    public DataTemplate TemplateB { get; set; } = DefaultItemTemplate;
+    public DataTemplate TemplateC { get; set; } = DefaultItemTemplate;
+
+    protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+    {
+        var myItem = (MyItem)item; 
+        return (myItem) switch
+        {
+            _ when myItem.Name.StartsWith("A") => TemplateA,
+            _ when myItem.Name.StartsWith("B") => TemplateB,
+            _ when myItem.Name.StartsWith("C") => TemplateC,
+            _ => TemplateDefault
+        };
+    }
+
+    public static DataTemplate DefaultItemTemplate = new DataTemplate(() =>
+    {
+        var label = new Label { VerticalOptions = LayoutOptions.Center };
+        label.Text = "!!!";
+        label.TextColor = Colors.Red;
+        return label;
+    });
+}
+

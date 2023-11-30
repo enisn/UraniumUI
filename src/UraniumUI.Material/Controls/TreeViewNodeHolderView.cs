c3ce5833c3fc7187678ca1183f7dd75294b4ddd7
Enis Necipoglu <iletisim@enisnecipoglu.com>
2023-11-30 22:02:43 +0300

    Update TreeViewNodeHolderView.cs

--- src/UraniumUI.Material/Controls/TreeViewNodeHolderView.cs
+++ src/UraniumUI.Material/Controls/TreeViewNodeHolderView.cs
@@ -218,7 +218,7 @@ public class TreeViewNodeHolderView : VerticalStackLayout
         }
     }
 
-    internal virtual void OnSelectedItemChanged()
+    protected virtual void OnSelectedItemChanged()
     {
         if (TreeView.SelectionMode == SelectionMode.Single)
         {

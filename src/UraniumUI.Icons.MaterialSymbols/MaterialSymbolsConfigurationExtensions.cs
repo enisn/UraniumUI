31ceeaed009e3954d628124a38c0f4428130eb2c
Dokug <mathias.garn@gmx.at>
2024-01-23 09:49:45 +0100

    Added Material Symbols font in new assembly

---
+++ src/UraniumUI.Icons.MaterialSymbols/MaterialSymbolsConfigurationExtensions.cs
@@ -0,0 +1,15 @@
+﻿namespace UraniumUI;
+
+public static class MaterialIconsConfigurationExtensions
+{
+    public static IFontCollection AddMaterialSymbolsFonts(this IFontCollection fonts)
+    {
+        var thisAssembly = typeof(MaterialIconsConfigurationExtensions).Assembly;
+
+        fonts.AddEmbeddedResourceFont(thisAssembly, "MaterialSymbolsOutlined.ttf", "MaterialSymbolsOutlined");
+        fonts.AddEmbeddedResourceFont(thisAssembly, "MaterialSymbolsRounded.ttf", "MaterialSymbolsRounded");
+        fonts.AddEmbeddedResourceFont(thisAssembly, "MaterialSymbolsSharp.ttf", "MaterialSymbolsSharp");
+
+        return fonts;
+    }
+}

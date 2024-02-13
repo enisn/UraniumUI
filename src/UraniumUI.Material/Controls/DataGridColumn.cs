98fad0ed52ea8d2427f8cc33aced49003a3e0662
Enis Necipoglu <info@enisnecipoglu.com>
2024-02-13 22:56:30 +0300

    Remove obsolete features from DataGrid

--- src/UraniumUI.Material/Controls/DataGridColumn.cs
+++ src/UraniumUI.Material/Controls/DataGridColumn.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel;
-using System.Reflection;
 
 namespace UraniumUI.Material.Controls;
 public class DataGridColumn
@@ -10,14 +9,8 @@ public class DataGridColumn
 
     public DataTemplate CellItemTemplate { get; set; }
 
-    [Obsolete("Use Binding property instead. Replace 'PropertyName=\"Name\"' with 'Binding=\"{Binding Name}\"'. Visit https://github.com/enisn/UraniumUI/blob/develop/docs/en/themes/material/components/DataGrid.md for the usage.")]
-    public string PropertyName { get; set; }
-
     public BindingBase Binding { get; set; }
 
     [TypeConverter(typeof(GridLengthTypeConverter))]
     public GridLength Width { get; set; } = GridLength.Auto;
-
-    [Obsolete]
-    internal PropertyInfo PropertyInfo { get; set; }
 }

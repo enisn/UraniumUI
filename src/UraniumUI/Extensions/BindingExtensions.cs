9f7d78c16b14d36c0d2f402e0093830d63e54cca
Enis Necipoglu <info@enisnecipoglu.com>
2024-02-16 15:05:56 +0300

    Create BindingExtensions.cs

---
+++ src/UraniumUI/Extensions/BindingExtensions.cs
@@ -0,0 +1,30 @@
+﻿namespace UraniumUI.Extensions;
+public static class BindingExtensions
+{
+    public static BindingBase CopyAsClone(this BindingBase binding)
+    {
+        if (binding is Binding b)
+        {
+            return new Binding(b.Path, b.Mode, b.Converter, b.ConverterParameter, b.StringFormat, b.Source);
+        }
+
+        if (binding is MultiBinding mb)
+        {
+            return new MultiBinding
+            {
+                Converter = mb.Converter,
+                ConverterParameter = mb.ConverterParameter,
+                StringFormat = mb.StringFormat,
+                Mode = mb.Mode,
+                Bindings = mb.Bindings.Select(CopyAsClone).ToList()
+            };
+        }
+
+        if (binding is BindingBase)
+        {
+            throw new NotSupportedException("Abstract classes or BindingBase types can not be cloned.");
+        }
+
+        return default;
+    }
+}

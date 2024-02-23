7f1b6620e13290ac29a6815379ef651d3a334c8a
Enis Necipoglu <info@enisnecipoglu.com>
2024-02-24 01:09:21 +0300

    Add AutoFormView as dialog in the sample page

--- demo/UraniumApp/ViewModels/AutoFormViewPageViewModel.cs
+++ demo/UraniumApp/ViewModels/AutoFormViewPageViewModel.cs
@@ -2,12 +2,29 @@
 using ReactiveUI;
 using ReactiveUI.Fody.Helpers;
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json;
+using System.Windows.Input;
+using UraniumUI.Dialogs;
 
 namespace UraniumApp.ViewModels;
 
 [RegisterAs(typeof(AutoFormViewPageViewModel))]
 public class AutoFormViewPageViewModel : ReactiveObject
 {
+    protected IDialogService _dialogService;
+    public AutoFormViewPageViewModel(IDialogService dialogService)
+    {
+        _dialogService = dialogService;
+        ShowDialogCommand = new Command(async () =>
+        {
+            var result = await _dialogService.DisplayFormViewAsync("Auto Form View", this);
+            if (result != null)
+            {
+                await _dialogService.DisplayViewAsync("Auto Form View Result", new Label { Text = JsonSerializer.Serialize(result) });
+            }
+        });
+    }
+
     [EmailAddress]
     [Required]
     [MinLength(5)]
@@ -20,4 +37,6 @@ public class AutoFormViewPageViewModel : ReactiveObject
 
     [Required]
     [Reactive] public bool IsTermsAndConditionsAccepted { get; set; }
+
+    public ICommand ShowDialogCommand { get; }
 }

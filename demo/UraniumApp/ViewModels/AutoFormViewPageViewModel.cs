27331601025d4b0f35f1da8a9c88a39c0dda96f3
Enis Necipoglu <info@enisnecipoglu.com>
2024-02-23 21:17:36 +0300

    Add AutoFormViewPage

---
+++ demo/UraniumApp/ViewModels/AutoFormViewPageViewModel.cs
@@ -0,0 +1,30 @@
+﻿using DotNurse.Injector.Attributes;
+using ReactiveUI;
+using ReactiveUI.Fody.Helpers;
+using System.ComponentModel.DataAnnotations;
+
+namespace UraniumApp.ViewModels;
+
+[RegisterAs(typeof(AutoFormViewPageViewModel))]
+public class AutoFormViewPageViewModel : ReactiveObject
+{
+    [EmailAddress]
+    [Required]
+    [MinLength(5)]
+    [Reactive]
+    public string Email { get; set; }
+    [Reactive]
+    public string FullName { get; set; }
+    [Reactive]
+    public Gender Gender { get; set; }
+    [Reactive]
+    public DateTime? BirthDate { get; set; }
+    [Reactive]
+    public TimeSpan? MeetingTime { get; set; }
+    [Reactive]
+    public int? NumberOfSeats { get; set; }
+
+    [Required]
+    [Reactive]
+    public bool IsTermsAndConditionsAccepted { get; set; }
+}
--- src/UraniumUI/Options/AutoFormViewOptions.cs
+++ src/UraniumUI/Options/AutoFormViewOptions.cs
@@ -1,9 +1,28 @@
-﻿using System.Reflection;
+﻿using InputKit.Shared.Validations;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
 
 namespace UraniumUI.Options;
-public class AutoFormViewOptions
+public sealed class AutoFormViewOptions
 {
     public Dictionary<Type, EditorForType> EditorMapping { get; } = new();
 
     public delegate View EditorForType(PropertyInfo property, object source);
+
+    public List<Action<View, PropertyInfo>> PostEditorActions { get; } = new();
+
+    public Func<PropertyInfo, string> PropertyNameFactory { get; set; } = DefaultPropertyNameFactory;
+
+    public Func<PropertyInfo, IEnumerable<IValidation>> ValidationFactory { get; set; }
+
+    private static string DefaultPropertyNameFactory(PropertyInfo property)
+    {
+        var attribute = property.GetCustomAttribute<DisplayAttribute>();
+        if (attribute != null)
+        {
+            return attribute.Name;
+        }
+
+        return property.Name;
+    }
 }

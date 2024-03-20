26c0fce50271f901144cd9b3ab22d228419f69c7
Enis Necipoglu <info@enisnecipoglu.com>
2024-03-20 20:06:29 +0300

    Create CommandExtensions.cs

---
+++ src/UraniumUI/Extensions/CommandExtensions.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Input;
+
+namespace UraniumUI.Extensions;
+public static class CommandExtensions
+{
+    public static bool TryExecute(this ICommand command, object parameter)
+    {
+        if (command is not null && command.CanExecute(parameter))
+        {
+            command.Execute(parameter);
+            return true;
+        }
+
+        return false;
+    }
+}

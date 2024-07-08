656957cfa797b7c95092c11e810f4d2168d5e880
enisn <info@enisnecipoglu.com>
2024-07-08 19:20:37 +0300

    Update PaginationSampleViewModel.cs

--- demo/UraniumApp/ViewModels/DataGrid/PaginationSampleViewModel.cs
+++ demo/UraniumApp/ViewModels/DataGrid/PaginationSampleViewModel.cs
@@ -101,7 +101,7 @@ public class Product
     public int id { get; set; }
     public string title { get; set; }
     public string description { get; set; }
-    public int price { get; set; }
+    public float price { get; set; }
     public float discountPercentage { get; set; }
     public float rating { get; set; }
     public int stock { get; set; }

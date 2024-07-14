422ebb89593911b11607ecf4370c6de7832a4818
enisn <info@enisnecipoglu.com>
2024-07-14 12:54:17 +0300

    Update demo of dropdown

--- demo/UraniumApp/Pages/DropdownFieldPage.xaml.cs
+++ demo/UraniumApp/Pages/DropdownFieldPage.xaml.cs
@@ -6,4 +6,9 @@ public partial class DropdownFieldPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void Button_Clicked(object sender, EventArgs e)
+    {
+        dropdown.SelectedItem = null;
+    }
 }

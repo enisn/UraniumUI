cdf6077aec12f9e5ce30710b1f2dd91e99e262f1
enisn <info@enisnecipoglu.com>
2024-09-26 13:45:18 +0300

    Fix codefactor issues

--- demo/UraniumApp/MauiProgram.cs
+++ demo/UraniumApp/MauiProgram.cs
@@ -69,13 +69,3 @@ public static class MauiProgram
         return builder.Build();
     }
 }
-
-public class MemoryLeakDetectEvents
-{
-    public event EventHandler<CollectionTarget> OnCollected;
-    public event EventHandler<CollectionTarget> OnLeaked;
-
-    internal void InvokeOnCollected(CollectionTarget target) => OnCollected?.Invoke(this, target);
-    internal void InvokeOnLeaked(CollectionTarget target) => OnLeaked?.Invoke(this, target);
-    
-}
